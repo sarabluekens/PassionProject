95bc719d4b1d13cedb08d0f1a64dfb54389028b1
sarabluekens <sara.bluekens@student.howest.be>
2020-12-05 14:13:57 +0100

    efforts for debugging multiple scenes

---
+++ week4/wouter-demo/classes/GameOverScene.js
@@ -0,0 +1,17 @@
+class GameOverScene {
+  constructor(engine, goToScene) {
+    this.engine = engine;
+    this.goToScene = goToScene;
+  }
+
+  start() {
+  }
+
+  stop() {
+  }
+
+  render() {
+  }
+}
+
+export default GameOverScene;
---
+++ week4/wouter-demo/classes/GameScene.js
@@ -0,0 +1,17 @@
+class GameScene {
+  constructor(engine, goToScene) {
+    this.engine = engine;
+    this.goToScene = goToScene;
+  }
+
+  start() {
+  }
+
+  stop() {
+  }
+
+  render() {
+  }
+}
+
+export default GameScene;
