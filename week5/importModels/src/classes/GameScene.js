75c7c182042752b4621ebea7f79aac6b3b726213
sarabluekens <sara.bluekens@student.howest.be>
2020-12-14 03:56:48 +0100

    importing models fixed

--- week5/importModels/src/classes/GameScene.js
+++ week5/importModels/src/classes/GameScene.js
@@ -5,9 +5,8 @@ class GameScene {
   constructor(engine, goToScene) {
     this.engine = engine;
     this.goToScene = goToScene;
-    this.playScene = null;
     this.box = null;
-    this.playerBox = null;
+    this.boem = false;
   }
 
     // this.goToScene('game')
@@ -18,52 +17,109 @@ class GameScene {
     let randomNumber = Math.random();
 
     if (randomNumber < 0.25) {
-        this.box = BABYLON.Mesh.CreateBox("Box1", 1.5, 0, this.playScene);
-        const material1 = new BABYLON.StandardMaterial("material1", this.playScene);
-        material1.emissiveColor = BABYLON.Color3.Red();
-        randomPosition(this.box);
-        this.box.material = material1;
-        console.log("1");
+      // this.box =  BABYLON.SceneLoader.ImportMeshAsync("", "./src/assets/models/", "speakers.glb", this.gameScene).newMeshes[1];
 
-        moveForward(this.box);
+        const gameScene = this.gameScene;
+        const box2 = new BABYLON.SceneLoader.ImportMesh("", "./src/assets/models/", "speakers.gltf", gameScene, function (newMeshes){
+          const playerBox = newMeshes[1];
+          const box = new BABYLON.TransformNode();
+          newMeshes.forEach(mesh => {
+            if(!mesh.parent) {
+              mesh.parent = box;
+            }
+
+            box.scaling = new BABYLON.Vector3(.5,.5,.5);
+            box.rotation = new BABYLON.Vector3 (0, 1.5, 0);
+            console.log(box);
+
+          });
+          randomPosition(box);
+          moveForward(box);
+          console.log(playerBox);
+          return playerBox;
+        });
+      // console.log(box2);
+      return box2;
+
     }
     else if (randomNumber <= 0.50) {
 
-      this.box = BABYLON.Mesh.CreateBox("Box2", 1.5, 0, this.playScene);
-        const material2 = new BABYLON.StandardMaterial("material1", this.playScene);
-        material2.emissiveColor = BABYLON.Color3.Blue();
-        this.box.material = material2;
-        randomPosition(this.box);
-        console.log("2");
+       const gameScene = this.gameScene;
+        new BABYLON.SceneLoader.ImportMesh("", "./src/assets/models/", "speakers.gltf", gameScene, function (newMeshes){
+          // const playerBox = newMeshes[1];
+          const box = new BABYLON.TransformNode();
+          newMeshes.forEach(mesh => {
+            if(!mesh.parent) {
+              mesh.parent = box;
+            }
 
+            box.scaling = new BABYLON.Vector3(.5,.5,.5);
+            box.rotation = new BABYLON.Vector3 (0, 1.5, 0);
+            console.log(box);
+          });
+          randomPosition(box);
+          moveForward(box);
 
-        moveForward(this.box);
+        });
+
+        const cube = new BABYLON.SceneLoader.ImportMesh(["Cube"], "./src/assets/models/", "speakers.gltf", gameScene, function (){});
+        console.log("cube");
+        return cube;
+        // console.log(box2);
+
+        // });
     }
+
     else if (randomNumber < 0.75) {
+      const gameScene = this.gameScene;
+      const box2 = new BABYLON.SceneLoader.ImportMesh("", "./src/assets/models/", "speakers.gltf", gameScene, function (newMeshes){
+        const playerBox = newMeshes[1];
+        const box = new BABYLON.TransformNode();
+        newMeshes.forEach(mesh => {
+          if(!mesh.parent) {
+            mesh.parent = box;
+          }
 
-      this.box = BABYLON.Mesh.CreateBox("Box3", 1, 0, this.playScene);
-        const material3 = new BABYLON.StandardMaterial("material1", this.playScene);
-        material3.emissiveColor = BABYLON.Color3.Green();
-        this.box.material = material3;
-        randomPosition(this.box);
+          box.scaling = new BABYLON.Vector3(.5,.5,.5);
+          box.rotation = new BABYLON.Vector3 (0, 1.5, 0);
+          console.log(box);
+        });
+        randomPosition(box);
+        moveForward(box);
+        console.log(playerBox);
+      return playerBox;
+      });
+      // console.log(box2);
+      return box2;
 
-        console.log("3");
-
-
-        moveForward(this.box);
     }
     else if (randomNumber >= 0.75) {
 
-      this.box = BABYLON.Mesh.CreateBox("Box4", .5, 0, this.playScene);
-        const material4 = new BABYLON.StandardMaterial("material1", this.playScene);
-        material4.emissiveColor = BABYLON.Color3.Yellow();
-        this.box.material = material4;
-        randomPosition(this.box);
+      const gameScene = this.gameScene;
 
-        console.log("4");
+      //  const box2 = function createNewMeshes (newMeshes) {
 
-        moveForward(this.box);
+      //   const playerBox = newMeshes[1];
+      //   const box = new BABYLON.TransformNode();
+      //   newMeshes.forEach(mesh => {
+      //     if(!mesh.parent) {
+      //       mesh.parent = box;
+      //     }
+      //     box.scaling = new BABYLON.Vector3(.5,.5,.5);
+      //     box.rotation = new BABYLON.Vector3 (0, 1.5, 0);
+      //     console.log(box);
+      //   });
+      //   randomPosition(box);
+      //   moveForward(box);
+      //   console.log(playerBox);
+      //   return playerBox;
+
+      // }
+      // new BABYLON.SceneLoader.ImportMesh("", "./src/assets/models/", "speakers.gltf", gameScene, createNewMeshes(newMeshes));
+
+      // return box2;
     }
+
 
     function randomPosition(box) {
         let randomNumberX = Math.random();
@@ -84,17 +140,7 @@ class GameScene {
           box.position = new BABYLON.Vector3(3, 1, 80);
           console.log("baan 3");
         }
-        return box;
       }
-
-      function checkCollisions (box){
-        console.log("test");
-        if (box.intersectsMesh(box, false)) {
-            console.log("boem");
-            //boem = false;
-        }
-    };
-
       function moveForward (box) {
         let start = Date.now();
         let timer = setInterval(() => {
@@ -102,24 +148,53 @@ class GameScene {
           let timePassed = Date.now() - start;
           if (timePassed > 10000) clearInterval(timer);
           box.position.z -= 1;
-          checkCollisions(box);
         }, 100);
 
       };
   }
+  checkCollisions (object){
+    console.log("test");
 
-  spawnRandomObstacles ( ) {
+    const object = this.spawnRandomObstacle();
+    const player = this.player();
+    if (player.intersectsMesh(object, false)) {
+        // boem = true;
+        console.log("boem");
+    }
+  };
 
+  player () {
+    const gameScene = this.gameScene;
+    const object = BABYLON.SceneLoader.ImportMesh("", "./src/assets/models/", "speakers.glb", gameScene, function (newMeshes){
+
+      // this.playerBox = newMeshes[1];
+      const player = new BABYLON.TransformNode();
+      newMeshes.forEach(mesh => {
+        if(!mesh.parent) {
+          mesh.parent = player;
+        }
+
+        player.position = new BABYLON.Vector3(0,0,-4);
+        player.scaling = new BABYLON.Vector3(.5,.5,.5);
+        player.rotation = new BABYLON.Vector3 (0, 1.5, 0);
+        console.log(player);
+      });
+      return;
+    });
+
+    return object;
+  };
+
+  spawnRandomModels ( ) {
       let spawnRate1 = 3000;
       let spawnRate2 = 1400;
 
-    const obstacle1 = setInterval(() => {this.spawnRandomObstacle()},  spawnRate1);
-    const obstacle2 = setInterval(() => {this.spawnRandomObstacle()},  spawnRate2);
-
+      this.obstacle1 = setInterval(() => {this.spawnRandomObstacle()},  spawnRate1);
+      this.obstacle2 = setInterval(() => {this.spawnRandomObstacle()},  spawnRate2);
   };
 
   sceneSetup () {
-    const gameScene = new BABYLON.Scene(this.engine);
+    const gameScene =  new BABYLON.Scene(this.engine);
     gameScene.clearColor = new BABYLON.Color3(0.5, 0.8, 0.5);
     // position of the camera
     var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 5, -10), gameScene);
@@ -131,21 +206,26 @@ class GameScene {
     camera.attachControl(canvas, true);
 
     // light
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), gameScene);
+
+    // testplayer
+    const boxBlue = BABYLON.Mesh.CreateBox("Box1", 1.5, 0, gameScene);
+    const material1 = new BABYLON.StandardMaterial("material1", gameScene);
+    material1.emissiveColor = BABYLON.Color3.Red();
+    boxBlue.material = material1;
+
+    // player
+
     //ground
-    const ground = BABYLON.MeshBuilder.CreateGround("ground", {height: 150, width: 9, subdivisions: 4});
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", {height: 150, width: 9, subdivisions: 4}, gameScene);
     ground.position = new BABYLON.Vector3(0,0,7);
 
     const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", gameScene);
     groundMaterial.emissiveColor = new BABYLON.Color3(0.85, 0.77, 0.60);
     ground.material = groundMaterial;
 
-    const boxBlue = BABYLON.Mesh.CreateBox("Box1", 1.5, 0, gameScene);
-    const material1 = new BABYLON.StandardMaterial("material1", gameScene);
-    material1.emissiveColor = BABYLON.Color3.Red();
-    boxBlue.material = material1;
-    //Add camera, light and meshes for scene1
-    this.spawnRandomObstacles();
+    this.spawnRandomModels();
+    this.player();
     return gameScene;
   }
 
@@ -153,18 +233,19 @@ class GameScene {
     const gameScene = this.sceneSetup();
     this.engine.runRenderLoop(() => {
       gameScene.render();
+      // this.checkCollisions();
 
     });
 
     setInterval(() => {
       this.goToScene("gameOver");
-    }, 5000)
+    }, 50000)
   }
 
   stop() {
     console.log("dit werkt");
-    clearInterval(obstacle1);
-    clearInterval(obstacle2);
+    clearInterval(this.obstacle1);
+    clearInterval(this.obstacle2);
     this.engine.stopRenderLoop(
     );
   };
