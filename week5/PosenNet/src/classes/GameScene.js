dc006c2eed7345fa09ccad83afa5bf435c9f58c3
sarabluekens <sara.bluekens@student.howest.be>
2020-12-15 19:00:32 +0100

    final changes

--- week5/PosenNet/src/classes/GameScene.js
+++ week5/PosenNet/src/classes/GameScene.js
@@ -1,7 +1,8 @@
 
 import * as BABYLON from '@babylonjs/core';
-import { Scene } from '@babylonjs/core';
-import "@babylonjs/loaders/glTF";
+import {Scene} from '@babylonjs/core';
+import '@babylonjs/loaders/glTF';
+
 
 class GameScene {
   constructor(engine, goToScene) {
@@ -14,188 +15,194 @@ class GameScene {
   }
 
   spawnRandomObstacle () {
-    let randomNumber = Math.random();
+    const randomNumber = Math.random();
+    const moveForward = box => {
+      const start = Date.now();
+      const timer = setInterval(() => {
+
+        const timePassed = Date.now() - start;
+        if (timePassed > 10000) clearInterval(timer);
+        box.position.z -= 0.1;
+        if (timePassed >= 10000) {
+          box.dispose();
+        }
+      }, 10);
+    };
+
+    const randomPosition = box => {
+      const randomNumberX = Math.random();
+      console.log(randomNumberX);
+
+      if (randomNumberX < 0.33 && randomNumberX > 0) {
+        // new BABYLON.Vector3(-3, 0, 40);
+        box.position = new BABYLON.Vector3(- 5, 0, 80);
+        console.log('baan 1');
+      }
+      else if (randomNumberX >= 0.33 && randomNumberX <= 0.66) {
+        // new BABYLON.Vector3(0, 0, 40);
+        box.position = new BABYLON.Vector3(0, 0, 80);
+        console.log('baan 2');
+      }
+      else if (randomNumberX > 0.66 && randomNumberX <= 1) {
+        // new BABYLON.Vector3(3, 0, 40);
+        box.position = new BABYLON.Vector3(5, 0, 80);
+        console.log('baan 3');
+      }
+    };
+
+    const checkCollisions = box => {
+      this.player();
+      console.log(box);
+      console.log('test');
+      if (this.player().intersectsMesh(this.spawnRandomObstacle(), false)) {
+        console.log('boem');
+      }
+    };
 
     if (randomNumber < 0.25) {
       // this.box =  BABYLON.SceneLoader.ImportMeshAsync("", "./src/assets/models/", "speakers.glb", this.gameScene).playerMeshes[1];
+      let obstacleBox;
+      const gameScene = this.gameScene;
+      const obstacle = new BABYLON.SceneLoader.ImportMesh('', './src/assets/models/', 'speakers.gltf', gameScene, objectMeshes => {
+        obstacleBox = objectMeshes[1];
+        const box = new BABYLON.TransformNode();
+        // this.boxes.playerBox = objectMeshes[1];
+        objectMeshes.forEach(mesh => {
+          if (!mesh.parent) {
+            mesh.parent = box;
+          }
 
-        const gameScene = this.gameScene;
-        const obstacle = new BABYLON.SceneLoader.ImportMesh("", "./src/assets/models/", "speakers.gltf", gameScene, objectMeshes =>{
-          obstacleBox = objectMeshes[1];
-          const box = new BABYLON.TransformNode();
-          // this.boxes.playerBox = objectMeshes[1];
-          objectMeshes.forEach(mesh => {
-            if(!mesh.parent) {
-              mesh.parent = box;
-            }
+          box.scaling = new BABYLON.Vector3(.5, .5, .5);
+          box.rotation = new BABYLON.Vector3 (0, 1.5, 0);
 
-            box.scaling = new BABYLON.Vector3(.5,.5,.5);
-            box.rotation = new BABYLON.Vector3 (0, 1.5, 0);
-            console.log(box);
 
-          });
-          randomPosition(box);
-          moveForward(box)
-          // checkCollisions(collisionBox)
+          console.log(box);
+
         });
-      // console.log(box2);
-      return
+        randomPosition(box);
+        moveForward(box);
+
+        return obstacleBox;
+      });
+      return obstacleBox;
 
     }
     else if (randomNumber <= 0.50) {
-
-       const gameScene = this.gameScene;
-        new BABYLON.SceneLoader.ImportMesh("", "./src/assets/models/", "speakers.gltf", gameScene, objectMeshes =>{
-          obstacle = objectMeshes[1];
-          const box = new BABYLON.TransformNode();
-          objectMeshes.forEach(mesh => {
-            if(!mesh.parent) {
-              mesh.parent = box;
-            }
-
-            box.scaling = new BABYLON.Vector3(.5,.5,.5);
-            box.rotation = new BABYLON.Vector3 (0, 1.5, 0);
-            console.log(box);
-          });
-          randomPosition(box);
-          moveForward(box);
-          // checkCollisions(collisionBox)
-
-        });
-
-        // console.log(box2);
-
-        // });
-    }
-
-    else if (randomNumber < 0.75) {
-      const gameScene = this.gameScene;
-      new BABYLON.SceneLoader.ImportMesh("", "./src/assets/models/", "speakers.gltf", gameScene,  objectMeshes =>{
-          obstacleBox = objectMeshes[1];
-        const box = new BABYLON.TransformNode();
-        objectMeshes.forEach(mesh => {
-          if(!mesh.parent) {
-            mesh.parent = box;
-          }
-
-          box.scaling = new BABYLON.Vector3(.5,.5,.5);
-          box.rotation = new BABYLON.Vector3 (0, 1.5, 0);
-          console.log(box);
-        });
-        randomPosition(box);
-        moveForward(box);
-        // checkCollisions(collisionBox)
-
-
-      });
-      // console.log(box2);
-
-
-    }
-    else if (randomNumber >= 0.75) {
       let obstacleBox;
       const gameScene = this.gameScene;
-      new BABYLON.SceneLoader.ImportMesh("", "./src/assets/models/", "speakers.gltf", gameScene, objectMeshes =>{
-
+      new BABYLON.SceneLoader.ImportMesh('', './src/assets/models/', 'speakers.gltf', gameScene, objectMeshes => {
         obstacleBox = objectMeshes[1];
         const box = new BABYLON.TransformNode();
         objectMeshes.forEach(mesh => {
-          if(!mesh.parent) {
+          if (!mesh.parent) {
             mesh.parent = box;
           }
-          box.scaling = new BABYLON.Vector3(.5,.5,.5);
+
+          box.scaling = new BABYLON.Vector3(.5, .5, .5);
           box.rotation = new BABYLON.Vector3 (0, 1.5, 0);
+
           console.log(box);
         });
         randomPosition(box);
         moveForward(box);
-        // checkCollisions(box);
+        checkCollisions(box);
 
         return obstacleBox;
       });
       return obstacleBox;
     }
 
-    function randomPosition(box) {
-        let randomNumberX = Math.random();
-        console.log(randomNumberX);
-
-        if (randomNumberX < 0.33 && randomNumberX > 0) {
-          // new BABYLON.Vector3(-3, 0, 40);
-          box.position = new BABYLON.Vector3(-5, 1, 80);
-          console.log("baan 1");
-        }
-        else if (randomNumberX >= 0.33 && randomNumberX <= 0.66) {
-          // new BABYLON.Vector3(0, 0, 40);
-          box.position = new BABYLON.Vector3(0, 1, 80);
-          console.log("baan 2");
-        }
-        else if (randomNumberX > 0.66 && randomNumberX <= 1) {
-          // new BABYLON.Vector3(3, 0, 40);
-          box.position = new BABYLON.Vector3(5, 1, 80);
-          console.log("baan 3");
-        }
-      }
-      function moveForward (box) {
-        let start = Date.now();
-        let timer = setInterval(() => {
-
-          let timePassed = Date.now() - start;
-          if (timePassed > 10000) clearInterval(timer);
-          box.position.z -= 0.1;
-          if(timePassed >= 10000){
-           box.dispose();
+    else if (randomNumber < 0.75) {
+      let obstacleBox;
+      const gameScene = this.gameScene;
+      new BABYLON.SceneLoader.ImportMesh('', './src/assets/models/', 'speakers.gltf', gameScene,  objectMeshes => {
+        obstacleBox = objectMeshes[1];
+        const box = new BABYLON.TransformNode();
+        objectMeshes.forEach(mesh => {
+          if (!mesh.parent) {
+            mesh.parent = box;
           }
-        }, 10);
-      };
 
+          box.scaling = new BABYLON.Vector3(.5, .5, .5);
+          box.rotation = new BABYLON.Vector3 (0, 1.5, 0);
+
+          console.log(box);
+        });
+        randomPosition(box);
+        moveForward(box);
+        // checkCollisions(collisionBox)
+        checkCollisions(box);
+
+
+        return obstacleBox;
+      });
+      return obstacleBox;
+
+    }
+    else if (randomNumber >= 0.75) {
+      let obstacleBox;
+      const gameScene = this.gameScene;
+      new BABYLON.SceneLoader.ImportMesh('', './src/assets/models/', 'speakers.gltf', gameScene, objectMeshes => {
+
+        obstacleBox = objectMeshes[1];
+        const box = new BABYLON.TransformNode();
+        objectMeshes.forEach(mesh => {
+          if (!mesh.parent) {
+            mesh.parent = box;
+          }
+          box.scaling = new BABYLON.Vector3(.5, .5, .5);
+          box.rotation = new BABYLON.Vector3 (0, 1.5, 0);
+
+          console.log(box);
+        });
+        randomPosition(box);
+        moveForward(box);
+        // checkCollisions(box);
+        checkCollisions(box);
+
+        return obstacleBox;
+      });
+      return obstacleBox;
+    }
   }
 
-  spawnRandomModels ( ) {
-      let spawnRate1 = 3000;
-      let spawnRate2 = 1400;
+  spawnRandomModels () {
+    const spawnRate1 = 3000;
+    const spawnRate2 = 1400;
 
-      this.obstacle1 = setInterval(() => {this.spawnRandomObstacle()},  spawnRate1);
-      this.obstacle2 = setInterval(() => {this.spawnRandomObstacle()},  spawnRate2);
-  };
+    this.obstacle1 = setInterval(() => {this.spawnRandomObstacle();},  spawnRate1);
+    this.obstacle2 = setInterval(() => {this.spawnRandomObstacle();},  spawnRate2);
+  }
 
-  player () {
-    const gameScene = this.gameScene;
-    let playerBox;
-    const model = new BABYLON.SceneLoader.ImportMesh("", "./src/assets/models/", "speakers.glb", gameScene, playerMeshes => {
-      playerBox = playerMeshes[1];
-      let player = new BABYLON.TransformNode();
-        playerMeshes.forEach(mesh => {
-          if(!mesh.parent) {
-            mesh.parent = player;
-          }
+  // player () {
+  //   const gameScene = this.gameScene;
+  //   let playerBox;
+  //   const model = new BABYLON.SceneLoader.ImportMesh('', './src/assets/models/', 'speakers.glb', gameScene, playerMeshes => {
+  //     playerBox = playerMeshes[1];
+  //     const player = new BABYLON.TransformNode();
+  //     playerMeshes.forEach(mesh => {
+  //       if (!mesh.parent) {
+  //         mesh.parent = player;
+  //       }
 
-        player.position = new BABYLON.Vector3(0,0,-4);
-        player.scaling = new BABYLON.Vector3(.5,.5,.5);
-        player.rotation = new BABYLON.Vector3 (0, 1.5, 0);
+  //       player.position = new BABYLON.Vector3(0, 0, - 4);
+  //       player.scaling = new BABYLON.Vector3(.5, .5, .5);
+  //       player.rotation = new BABYLON.Vector3 (0, 1.5, 0);
 
 
-      });
-      return playerBox;
-    });
-    return playerBox;
-  };
+  //     });
+  //     return playerBox;
+  //   });
+  // //   return playerBox;
+  // }
 
-  checkCollisions() {
-    this.player();
-    console.log(box);
-    console.log("test");
-    if (this.player().intersectsMesh(this.spawnRandomObstacle(), false)) {
-        console.log("boem");
-        boem = false;
-    }
-}
+
   sceneSetup () {
     const gameScene =  new BABYLON.Scene(this.engine);
     gameScene.collisionsEnabled = true;
     gameScene.clearColor = new BABYLON.Color3(1, 0.4, 0.4);
     // position of the camera
-    var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 5, -10), gameScene);
+    const camera = new BABYLON.UniversalCamera('UniversalCamera', new BABYLON.Vector3(0, 5, - 10), gameScene);
 
     //  where does the camera rotate to? in dit geval richt hij zich naar het middelpunt van de canvas
     camera.setTarget(BABYLON.Vector3.Zero());
@@ -204,20 +211,35 @@ class GameScene {
     camera.attachControl(canvas, true);
 
     // light
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), gameScene);
+
+
+    const model = new BABYLON.SceneLoader.ImportMesh('', './src/assets/models/', 'speakers.glb', gameScene, playerMeshes => {
+      playerBox = playerMeshes[1];
+      const player = new BABYLON.TransformNode();
+      playerMeshes.forEach(mesh => {
+        if (!mesh.parent) {
+          mesh.parent = player;
+        }
+
+        player.position = new BABYLON.Vector3(0, 0, - 4);
+        player.scaling = new BABYLON.Vector3(.5, .5, .5);
+        player.rotation = new BABYLON.Vector3 (0, 1.5, 0);
+      });
+      model.playerBox;
+    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(1, 1, 0), gameScene);
 
     // testplayer
-    const boxBlue = BABYLON.Mesh.CreateBox("Box1", 1.5, 0, gameScene);
-    const material1 = new BABYLON.StandardMaterial("material1", gameScene);
+    const boxBlue = BABYLON.Mesh.CreateBox('Box1', 1.5, 0, gameScene);
+    const material1 = new BABYLON.StandardMaterial('material1', gameScene);
     material1.emissiveColor = BABYLON.Color3.Red();
     boxBlue.material = material1;
 
     //ground
-    const ground = BABYLON.MeshBuilder.CreateGround("ground", {height: 150, width: 20, subdivisions: 4}, gameScene);
-    ground.position = new BABYLON.Vector3(0,0,7);
+    const ground = BABYLON.MeshBuilder.CreateGround('ground', {height: 150, width: 20, subdivisions: 4}, gameScene);
+    ground.position = new BABYLON.Vector3(0, 0, 7);
 
-    var wood = new BABYLON.StandardMaterial("wood", gameScene);
-    wood.diffuseTexture = new BABYLON.Texture("./src/assets/img/wood2.jpg", gameScene);
+    const wood = new BABYLON.StandardMaterial('wood', gameScene);
+    wood.diffuseTexture = new BABYLON.Texture('./src/assets/img/wood2.jpg', gameScene);
     // const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", gameScene);
     // groundMaterial.emissiveColor = new BABYLON.Color3(0.85, 0.77, 0.60);
     ground.material = wood;
@@ -227,7 +249,7 @@ class GameScene {
       wood.diffuseTexture.vOffset = t;
       t += 0.0015;
 
-  });
+    });
     this.spawnRandomModels();
     this.player();
     return gameScene;
@@ -235,23 +257,24 @@ class GameScene {
 
   start() {
     const gameScene = this.sceneSetup();
+
     this.engine.runRenderLoop(() => {
       gameScene.render();
 
     });
 
     setInterval(() => {
-      this.goToScene("gameOver");
-    }, 50000)
+      this.goToScene('gameOver');
+    }, 50000);
   }
 
   stop() {
-    console.log("dit werkt");
+    console.log('dit werkt');
     clearInterval(this.obstacle1);
     clearInterval(this.obstacle2);
     this.engine.stopRenderLoop(
     );
-  };
+  }
 
 
 }
--- week5/PosenNet/src/index.js
+++ week5/PosenNet/src/index.js
@@ -7,9 +7,12 @@ import TutorialFilmpje from './classes/TutorialFilmpje.js';
 import {Engine} from '@babylonjs/core/Engines/engine';
 import * as BABYLON from '@babylonjs/core';
 
+import * as posenet from '@tensorflow-models/posenet';
+import '@tensorflow/tfjs-backend-webgl';
+import * as tf from '@tensorflow/tfjs-core';
+
 import "@babylonjs/loaders/glTF";
 import './css/style.css';
-
 
 let startScene;
 let gameScene;
@@ -20,8 +23,79 @@ let currentScene;
 let tutorial;
 let engine;
 let canvas;
+  // ------------------------------------ globaal ------------------------------------
+  const $webcam = document.getElementById("webcam");
+
+  const leftEyeX = document.createElement("p");
+  leftEyeX.classList.add("xCO");
+  leftEyeX.textContent = '';
+
+  const leftEyeY = document.createElement("p");
+  leftEyeY.classList.add("yCO");
+  leftEyeY.textContent = '';
+
+  let xValue;
+  let player;
+
+   // ------------------------------------ camera ------------------------------------
+
+   const camera = async () => {
+    requestAnimationFrame(render);
+    const options = { audio: false, video: { width: 320, height: 180} };
+    let stream = await navigator.mediaDevices.getUserMedia(options);
+    $webcam.srcObject = stream;
+
+    if(!$webcam.captureStream) {
+        $webcam.captureStream = () => stream;
+    }
+}
+
+const render = (time) => {
+    time *= 0.001;
+    requestAnimationFrame(render);
+}
+
+// ------------------------------------ posenet ------------------------------------
+
+    const runPoseNet = async () => {
+        await camera();
+        const net = await posenet.load({
+        inputResolution:{width:640, height:480},
+        scale: 0.5
+        });
+
+        setInterval(() => {
+            detect(net );
+        }, 100 );
+    };
+
+const detect = async (net) => {
+    //make detections
+    const pose = await net.estimateSinglePose(webcam);
+    // console.log(pose);
+
+    const outputX = pose.keypoints[1].position.x;
+    const outputY = pose.keypoints[1].position.y;
+
+    if (outputX >= 200 && outputX < 350) {
+        gameScene.player.postion.z = 3;
+        } else if (outputX >= 130 && outputX < 200){
+            xValue = 0
+            } else if (outputX >= 0 && outputX < 130){
+                xValue = -3;
+    };
+
+    leftEyeX.textContent = outputX
+    leftEyeY.textContent = outputY;
+    document.body.appendChild(leftEyeX);
+    document.body.appendChild(leftEyeY);
+
+    return xValue ;
+}
 
 const init = () => {
+  camera();
+  tf.ready( runPoseNet(gameScene));
   canvas = document.getElementById('canvas');
   engine = new BABYLON.Engine(canvas, true);
   startScene = new StartScene(engine, goToScene);
